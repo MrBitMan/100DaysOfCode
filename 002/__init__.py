6d4acbab964a069449dca173a6870323eb701c82
gkhan496 <inamato49609@gmail.com>
2019-01-02 19:30:54 +0300

    02.01.2019

--- 002/__init__.py
+++ 002/__init__.py
@@ -1,1 +1,2 @@
 __author__ = 'PyBites'
+__author__ = 'gkhan496'
--- 002/get_ips.py
+++ 002/get_ips.py
@@ -1,7 +1,5 @@
-#!python3
-#get_ips.py is a script to print out the valid IPs of a specified network
-
 import ipaddress
+import os
 
 print("Enter a network in the following format xxx.xxx.xxx.0/xx:")
 network = input()
@@ -10,5 +8,13 @@ netobj = ipaddress.ip_network(network)
 for i in netobj.hosts():
 	print(i)
 
-print('\nHit enter to escape.')
-input()
+print("Enter the address you want to ping")
+ping = input()
+hostname = ping
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+if response == 0:
+  print(hostname, 'is up!')
+else:
+  print(hostname, 'is down!')
