d0452230a672a695207e9fa2b133a4c023bc2af5
gkhan496 <inamato49609@gmail.com>
2019-01-06 14:07:28 +0300

    06.01.2019

--- 006/print_dict.py
+++ 006/print_dict.py
@@ -4,6 +4,7 @@
 some_dict = {"Julian": 25, "Bob": 26, "Dan": 47, "Cornelius": 3}
 
 def print_dict(dictionary):
+	print("Keys : ",dictionary.keys(),"Values : ",dictionary.values())
 	for k, v in dictionary.items():
 		print('\n{}: {}'.format(k, v))	
 
