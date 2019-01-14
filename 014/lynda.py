15d58edd8e59a65c24a264be6e5d3bac5c5f2d75
gkhan496 <gguzelkokar.mdbf15@iste.edu.tr>
2019-01-14 23:56:45 +0300

    14.01.2019

--- 014/lynda.py
+++ 014/lynda.py
@@ -46,3 +46,9 @@ if __name__ == '__main__':
 
     for tweet in get_tweets(grep):
         tweet_status(tweet)
+"""Universal Feed Parser is a Python module 
+for downloading and parsing syndicated feeds.
+ It can handle RSS 0.90, Netscape RSS 0.91, Userland RSS 0.91, 
+ RSS 0.92, RSS 0.93, RSS 0.94, RSS 1.0, RSS 2.0, Atom 0.3, Atom 1.0, 
+ and CDF feeds. It also parses several popular extension 
+modules, including Dublin Core and Apple’s iTunes extensions."""
