830849d6d2b896911772b907b3e4fcf82352b597
gkhan496 <gguzelkokar.mdbf15@iste.edu.tr>
2019-01-19 23:10:10 +0300

    19.01.2019

--- 019/number_sorter.py
+++ 019/number_sorter.py
@@ -6,7 +6,7 @@ import pyperclip
 num_list = []
 
 def create_list():
-	input_list = pyperclip.paste().split('\n')
+	input_list = pyperclip.paste().split()
 	input_list.sort(key=float)
 	return input_list
 
@@ -14,7 +14,8 @@ def create_list():
 def copy_back(num_list):
 	pyperclip.copy('\n'.join(num_list))
 
-
+## 4 3 2 6 7 9 4 10 ctrl+c numbers :D
 if __name__ == "__main__":
 	num_list = create_list()
 	copy_back(num_list)
+	print(pyperclip.paste())
